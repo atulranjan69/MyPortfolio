4f5d8a31dce8a3581128d7d6e9453361138c931f
sonu sharma <sonusharma@gmail.com>
2025-07-24 00:59:19 +0530

    Fix vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   base: '/MyPortfolio/',
+// //   plugins: [react()],
+// // });
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
+// // The 'base' option ensures that all links and assets are correctly resolved
+// // when deploying to a subdirectory like '/MyPortfolio/'.
+// // This makes your deployed project behave the same as on localhost.
 // export default defineConfig({
 //   base: '/MyPortfolio/',
 //   plugins: [react()],
@@ -9,11 +19,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// The 'base' option ensures that all links and assets are correctly resolved
-// when deploying to a subdirectory like '/MyPortfolio/'.
-// This makes your deployed project behave the same as on localhost.
 export default defineConfig({
-  base: '/MyPortfolio/',
+  base: './',  // <-- Ye change karo
   plugins: [react()],
-});
+})
 
